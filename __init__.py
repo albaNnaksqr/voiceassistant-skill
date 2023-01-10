1cc8b5285a23c3e0eb06415e16268b3fe10d34bc
Easy Shopping <1045907603s@gmail.com>
2023-01-10 21:58:31 +0800

    update

--- __init__.py
+++ __init__.py
@@ -1,11 +1,11 @@
-from mycroft import MycroftSkill, intent_file_handler
-
+from mycroft import MycroftSkill, intent_handler
+from adapt.intent import IntentBuilder
 
 class Voiceassistant(MycroftSkill):
     def __init__(self):
         MycroftSkill.__init__(self)
 
-    @intent_file_handler('voiceassistant.intent')
+    @intent_handler(IntentBuilder('voiceassistant').require('Assistant').build())
     def handle_voiceassistant(self, message):
         self.speak_dialog('voiceassistant')
 
